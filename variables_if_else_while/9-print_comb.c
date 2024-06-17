89fc7c44ae52075e0049f4088686af6c15b617d5
lucasnielc <lucasolegniel@gmail.com>
2024-06-17 16:58:35 +0200

    program that prints all possible combinations of single-digit numbers.

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print all the numbers of base 16 in lowercase'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48  ; n < 58 ; n++)
+	{
+		putchar(n);
+		if (n < 57)
+		{
+		putchar(44);
+		putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
