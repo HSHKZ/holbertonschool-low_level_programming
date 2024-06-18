0a3032a233a37c04ff504dbee7fa97f9796a0fd8
lucasnielc <lucasolegniel@gmail.com>
2024-06-18 15:18:46 +0200

    program that prints _putchar, followed by a new line.

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,32 @@
 #include "main.h"
-/**
- *main - Write a program that prints _putchar, followed by a new line.
- *
- *Description: affiche les caractères les un après les autres
- *puis retour à la ligne.
- *
- *Return: all thing
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}	
+    char *str = "_putchar";
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+
+    return (0);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
