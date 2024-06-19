37c1a6a43a8161cd8c891d05cbd238b1f6839c36
lucasnielc <lucasolegniel@gmail.com>
2024-06-19 09:28:19 +0200

    program that prints _putchar, followed by a new line.

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,7 @@
-#include "main.h"
+#include "stdio.h"
 
-/**
- * main - prints "_putchar" followed by a new line, and exits.
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	char message[] = "_putchar\n";
-	unsigned long int index;
-
-	for (index = 0; index < sizeof(message); index++)
-	{
-		_putchar(message[index]);
-	}
-
-	return (0);
+	printf("_putchar\n");
+	return 0;
 }
