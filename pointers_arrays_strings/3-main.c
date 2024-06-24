5f27cd5fa5d657324668406ca20b4b36c41ce214
lucasnielc <lucasolegniel@gmail.com>
2024-06-24 15:57:10 +0200

    function that prints a string, followed by a new line, to stdout.

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main -check the code
+ * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 
 int main(void)
+
 {
 	char *str;
 
--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
- * _puts - check the code
+ * _puts - prints a string, followed by a new line, to stdout
  *
- * Description: print
- *
- * @str: input
- *
- * Return: Always 0.
+ * @str: the string to print
  */
 
 void _puts(char *str)
-{
-	int i;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
-		_putchar(str[i]);
-	_putchar('\n');
+{
+	while (*str)
+	{
+		write(1, str, 1)
+		str++;
+	}
+	write(1, "\n", 1)
 }
