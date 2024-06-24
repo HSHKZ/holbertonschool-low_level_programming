0cfbc9be45acfb056557795912962871d83673c6
lucasnielc <lucasolegniel@gmail.com>
2024-06-24 16:44:20 +0200

    function that prints a string, followed by a new line, to stdout.

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,17 +1,32 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * rev_string - Reverses a string.
  *
- * Return: Always 0
+ * @s: The string to be reversed.
+ *
+ * Return: void
  */
 
-int main(void)
+void rev_string(char *s)
 
 {
-	char *str;
+    int length = 0;
+    int i;
+    char temp;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	return (0);
+    // Find the length of the string
+    while (s[length] != '\0')
+
+    {
+        length++;
+    }
+
+    // Swap the characters from the start and end of the string
+    for (i = 0; i < length / 2; i++)
+    {
+        temp = s[i];
+        s[i] = s[length - 1 - i];
+        s[length - 1 - i] = temp;
+    }
 }
--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,19 +1,33 @@
+#include <stdio.h>
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _puts - prints a string, followed by a new line, to stdout
+ * rev_string - Reverses a string.
  *
- * @str: the string to print
+ * @s: The string to be reversed.
+ *
+ * Return: void
  */
 
-void _puts(char *str)
+void rev_string(char *s)
 
 {
-	while (*str)
-	{
-		write(1, str, 1)
-		str++;
-	}
-	write(1, "\n", 1)
+    int length = 0;
+    int i;
+    char temp;
+
+    // Find the length of the string
+    while (s[length] != '\0')
+
+    {
+        length++;
+    }
+
+    // Swap the characters from the start and end of the string
+    for (i = 0; i < length / 2; i++)
+    {
+        temp = s[i];
+        s[i] = s[length - 1 - i];
+        s[length - 1 - i] = temp;
+    }
 }
