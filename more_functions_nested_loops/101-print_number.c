95a588f5cc6c512fffe7bde0db49e2ebe99103cb
lucasnielc <lucasolegniel@gmail.com>
2024-06-27 18:03:37 +0200

    function that prints an integer.

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "mainadvanced.h"
+
+/**
+ * print_number - check the code
+ *
+ * Description: print
+ *
+ * @n: input
+ *
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n > 9)
+		print_number(n / 10);
+	_putchar('0' + (n % 10));
+}
---
+++ more_functions_nested_loops/mainadvanced.h
@@ -0,0 +1,2 @@
+int _putchar(char a);
+void print_number(int n);
