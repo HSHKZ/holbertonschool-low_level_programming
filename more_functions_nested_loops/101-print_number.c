74f8b28094302de6c7a58a6e68d631ce807eb2b7
lucasnielc <lucasolegniel@gmail.com>
2024-06-27 18:45:45 +0200

    Add print_number function and supporting files

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -3,15 +3,11 @@
 
 /**
  * print_number - prints an integer using only _putchar
- *
  * @n: integer to print
  */
-
 void print_number(int n)
-
 {
     if (n == INT_MIN)
-
     {
         _putchar('-');
         _putchar('2');
@@ -20,17 +16,13 @@ void print_number(int n)
     }
 
     if (n < 0)
-
     {
         _putchar('-');
         n = -n;
     }
 
     if (n / 10)
-
-    {
-	    print_number(n / 10);
-    }
+        print_number(n / 10);
 
     _putchar((n % 10) + '0');
 }
