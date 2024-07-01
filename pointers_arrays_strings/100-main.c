6e4d87ebaa57b48023d818b2243a9e4c3efacfed
lucasnielc <lucasolegniel@gmail.com>
2024-07-01 14:44:13 +0200

    function that sets the value of a pointer to a char.

--- pointers_arrays_strings/100-main.c
+++ pointers_arrays_strings/100-main.c
@@ -1,43 +1,18 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _atoi - check the code
- *
- * Description: print
- *
- * @s: input
+ * main - check the code
  *
  * Return: Always 0.
  */
-
-int _atoi(char *s)
+int main(void)
 {
-	unsigned int number = 0;
-	int negative = 0;
-	int i;
-	int stillNumbers = 1;
-	int backToOthers = 0;
+    char *s0 = "Bob Dylan";
+    char *s1 = "Robert Allen";
 
-	for (i = 0 ; s[i] != '\0' ; i++)
-	{
-		if ((s[i] == '-') && (stillNumbers == 1))
-		{
-			if (negative == 1)
-				negative = 0;
-			else
-				negative = 1;
-		}
-		else if ((s[i] >= '0') && (s[i] <= '9') && (stillNumbers == 1))
-		{
-			number = (number * 10) + s[i] - '0';
-			backToOthers = 1;
-		}
-		else if (backToOthers == 1)
-			stillNumbers = 0;
-	}
-	if (negative == 1)
-		number *= -1;
-
-	return (number);
+    printf("%s, %s\n", s0, s1);
+    set_string(&s1, s0);
+    printf("%s, %s\n", s0, s1);
+    return (0);
 }
