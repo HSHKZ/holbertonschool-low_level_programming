e478b3e256a634425a16dc0ace9d62a1bc6dd9e2
HSHKZ <lucasolegniel@gmail.com>
2024-07-02 20:25:59 +0200

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,6 +1,43 @@
 #include "main.h"
 
 /**
+ * wildcmp_recursive - recursively compare two strings with wildcard support
+ * @s1: first string to compare
+ * @s2: second string, may contain the wildcard '*'
+ *
+ * Description:
+ * - Helper function for wildcmp, without using loops.
+ *
+ * Return: 1 if the strings can be considered identical, 0 otherwise
+ */
+
+int wildcmp_recursive(char *s1, char *s2)
+
+{
+    if (*s1 == '\0' && *s2 == '\0')
+        return 1;
+
+    if (*s2 == '*')
+    {
+        if (*(s2 + 1) == '\0')
+            return 1;
+        
+        if (*s1 == '\0')
+            return 0;
+
+        return (wildcmp_recursive(s1, s2 + 1) || wildcmp_recursive(s1 + 1, s2));
+    }
+
+    if (*s1 == '\0' || *s2 == '\0')
+        return 0;
+
+    if (*s1 == *s2 || *s2 == '?')
+        return wildcmp_recursive(s1 + 1, s2 + 1);
+
+    return 0;
+}
+
+/**
  * wildcmp - compare two strings with wildcard support
  * @s1: first string to compare
  * @s2: second string, may contain the wildcard '*'
@@ -15,31 +52,5 @@
 int wildcmp(char *s1, char *s2)
 
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return 1;
-
-    if (*s2 == '*')
-    {
-        while (*s2 == '*')
-            s2++;
-
-        if (*s2 == '\0')
-            return 1;
-
-        while (*s1 != '\0')
-        {
-            if (wildcmp(s1, s2))
-                return 1;
-            s1++;
-        }
-        return 0;
-    }
-
-    if (*s1 == '\0')
-        return 0;
-
-    if (*s1 == *s2 || *s2 == '?')
-        return wildcmp(s1 + 1, s2 + 1);
-
-    return 0;
+    return wildcmp_recursive(s1, s2);
 }
