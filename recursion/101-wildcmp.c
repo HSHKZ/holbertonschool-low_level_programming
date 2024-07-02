95607aad9ff76424a320e3795686579040b11a92
HSHKZ <lucasolegniel@gmail.com>
2024-07-02 18:06:07 +0200

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -2,24 +2,29 @@
 
 /**
  * wildcmp - compares two strings and returns 1 if they can be considered
+ *
  * identical, otherwise returns 0
+ *
  * @s1: the first string
+ *
  * @s2: the second string which can contain the special character *
  *
  * Return: 1 if strings are identical, 0 otherwise
  */
+
 int wildcmp(char *s1, char *s2)
+
 {
  if (*s2 == '\0')
-     return (*s1 == '\0');
+ 	return (*s1 == '\0');
  if (*s2 == '*')
  {
-     if (*(s2 + 1) == '\0' || wildcmp(s1, s2 + 1))
- 	return (1);
-     if (*s1 != '\0' && wildcmp(s1 + 1, s2))
- 	return (1);
+ 	if (*(s2 + 1) == '\0' || wildcmp(s1, s2 + 1))
+ 		return (1);
+ 	if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+ 		return (1);
  }
  if (*s1 == *s2)
-     return (wildcmp(s1 + 1, s2 + 1));
+ 	return (wildcmp(s1 + 1, s2 + 1));
  return (0);
 }
