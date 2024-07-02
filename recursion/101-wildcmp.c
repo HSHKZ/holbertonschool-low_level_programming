0ed21667418e107ab717da481171fff56bd376cb
HSHKZ <lucasolegniel@gmail.com>
2024-07-02 20:32:30 +0200

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -10,31 +10,29 @@
  *
  * Return: 1 if the strings can be considered identical, 0 otherwise
  */
-
 int wildcmp_recursive(char *s1, char *s2)
-
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return 1;
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
 
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '\0')
-            return 1;
-        
-        if (*s1 == '\0')
-            return 0;
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '\0')
+return (1);
+		
+if (*s1 == '\0')
+return (0);
 
-        return (wildcmp_recursive(s1, s2 + 1) || wildcmp_recursive(s1 + 1, s2));
-    }
+return (wildcmp_recursive(s1, s2 + 1) || wildcmp_recursive(s1 + 1, s2));
+}
 
-    if (*s1 == '\0' || *s2 == '\0')
-        return 0;
+if (*s1 == '\0' || *s2 == '\0')
+return (0);
 
-    if (*s1 == *s2 || *s2 == '?')
-        return wildcmp_recursive(s1 + 1, s2 + 1);
+if (*s1 == *s2 || *s2 == '?')
+return (wildcmp_recursive(s1 + 1, s2 + 1));
 
-    return 0;
+return (0);
 }
 
 /**
@@ -48,9 +46,7 @@ int wildcmp_recursive(char *s1, char *s2)
  *
  * Return: 1 if the strings can be considered identical, 0 otherwise
  */
-
 int wildcmp(char *s1, char *s2)
-
 {
-    return wildcmp_recursive(s1, s2);
+return (wildcmp_recursive(s1, s2));
 }
