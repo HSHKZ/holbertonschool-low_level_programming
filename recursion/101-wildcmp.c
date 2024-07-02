60e00c46c6956815bce8a59c3f28d3fd84d00c0f
HSHKZ <lucasolegniel@gmail.com>
2024-07-02 18:02:58 +0200

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,78 +1,25 @@
 #include "main.h"
 
 /**
- * isIdentical - searches for the length of the string
+ * wildcmp - compares two strings and returns 1 if they can be considered
+ * identical, otherwise returns 0
+ * @s1: the first string
+ * @s2: the second string which can contain the special character *
  *
- * Description: print
- *
- * @lenFirst: length string 1
- *
- * @lenSecond: length string 2
- *
- * @String1: string 1
- *
- * @String2: string 2
- *
- * Return: Always 0.
+ * Return: 1 if strings are identical, 0 otherwise
  */
-
-int isIdentical(char *String1, char *String2)
-{
-	if ((String1[0] == '\0') && (String2[0] == '\0'))
-		return (1);
-	else if ((String1[0] != String2[0]) && (String2[0] != '*'))
-		return (0);
-	else if (String2[0] != '*')
-		return (isIdentical(String1 + 1, String2 + 1));
-	else if (String2[1] == String1[0])
-		return (isIdentical(String1, String2 + 1));
-	else if (String2[1] == String1[1])
-		return (isIdentical(String1 + 1, String2 + 1));
-	else if (String2[1] == '*')
-		return (isIdentical(String1, String2 + 1));
-	else if ((String1[0] == '\0') || (String2[0] == '\0'))
-		return (0);
-	else 
-		return (isIdentical(String1 + 1, String2));
-}
-
-/**
- * lengthString - searches for the length of the string
- *
- * Description: print
- *
- * @s: input string
- *
- * Return: Always 0.
- */
-
-/*
-int lengthString(char *s)
-{
-	if (s[0] == '\0')
-		return (0);
-	return (1 + lengthString(s + 1));
-}
-*/
-
-/**
- * wildcmp - check the code
- *
- * Description: print
- *
- * @s1: input string 1
- *
- * @s2: input string 2
- *
- * Return: Always 0.
- */
-
 int wildcmp(char *s1, char *s2)
 {
-/*	int i, j;
-
-	i = lengthString(s1) - 1;
-	j = lengthString(s2) - 1;
-*/
-	return (isIdentical(s1, s2)); 
+ if (*s2 == '\0')
+     return (*s1 == '\0');
+ if (*s2 == '*')
+ {
+     if (*(s2 + 1) == '\0' || wildcmp(s1, s2 + 1))
+ 	return (1);
+     if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+ 	return (1);
+ }
+ if (*s1 == *s2)
+     return (wildcmp(s1 + 1, s2 + 1));
+ return (0);
 }
