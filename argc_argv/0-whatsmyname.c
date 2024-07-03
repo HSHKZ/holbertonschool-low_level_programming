1aa9b3d5d54ff6416fcd5e4a588cc7da7ef370d5
HSHKZ <lucasolegniel@gmail.com>
2024-07-03 09:56:34 +0200

    program that prints its name, followed by a new line.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - begining
+ *
+ * Description: print if negative or positive
+ *
+ * @argc: number of arguments in argv
+ *
+ * @argv: array of arguments
+ *
+ * Return: 0 ends the program
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
