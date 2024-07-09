37b3ef4d15725fb95e51b3384eb186e628eaed10
HSHKZ <lucasolegniel@gmail.com>
2024-07-09 09:24:51 +0200

    function that returns a pointer to a 2 dimensional array of integers.

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,54 +1,39 @@
-#include<stdlib.h>
-#include"main.h"
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * alloc_grid - check the code
+ * alloc_grid - begining
  *
- * @width: The character to print
+ * Description: print if negative or positive
  *
- * @height: The character to print
+ * @width: width of array
  *
- * Return: Always tableau.
+ * @height: height of array
+ *
+ * Return: 0 ends the program
  */
 
 int **alloc_grid(int width, int height)
-
 {
-	int **tableau;
-	int i, j;
+	int **array, i, j;
 
-	if (width <= 0 || height <= 0)
-	
-	{
+	if ((width <= 0) || (height <= 0))
 		return (NULL);
-	}
-	
-	tableau = malloc(sizeof(int *) * height);
-	if (tableau == NULL)
-		
-	{
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
 		return (NULL);
-	}
-	
 	for (i = 0 ; i < height ; i++)
-
 	{
-		tableau[i] = malloc(sizeof(int) * width);
-		if (tableau[i] == NULL)
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
 		{
-			for ( ; i >= 0 ; i--)
-			{
-				free(tableau[i]);
-			}
-			free(tableau);
+			for (j = 0 ; j < i ; j++)
+				free(array[j]);
+			free(array);
 		}
-		
 		for (j = 0 ; j < width ; j++)
-			
-		{
-			tableau[i][j] = 0;
-		}
+			array[i][j] = 0;
 	}
-	
-	return (tableau);
+	return (array);
 }
