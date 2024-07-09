d7f2cb190571a5116a842c27d41fe6d81fe194f1
HSHKZ <lucasolegniel@gmail.com>
2024-07-09 09:40:54 +0200

    C - More malloc, free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - prints a grid of integers
+ * @b: The character to print
+ * Return: Nothing.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
