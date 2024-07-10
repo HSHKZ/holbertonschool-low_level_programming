27f03db2d2c79e24229c1164ca4acc834bd0a532
HSHKZ <lucasolegniel@gmail.com>
2024-07-10 14:15:24 +0200

    program that multiplies two positive numbers.

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -13,6 +13,7 @@ void print_error_and_exit(void)
 /**
  * is_valid_number - Checks if a string contains only digits.
  * @str: The string to check
+ *
  * Return: 1 if valid number, 0 otherwise
  */
 int is_valid_number(char *str)
@@ -30,6 +31,7 @@ int is_valid_number(char *str)
  * multiply_strings - Multiplies two strings representing numbers.
  * @num1: The first number as a string
  * @num2: The second number as a string
+ *
  * Return: The result of num1 * num2 as a string
  */
 char *multiply_strings(char *num1, char *num2)
@@ -79,6 +81,7 @@ char *multiply_strings(char *num1, char *num2)
  * main - Entry point of the program
  * @argc: Number of arguments
  * @argv: Array of argument strings
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
