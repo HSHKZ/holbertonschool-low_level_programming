5c2ebbb671f2da79e81c1a84d79f1d7c9b5471aa
HSHKZ <lucasolegniel@gmail.com>
2024-07-10 15:07:22 +0200

    function that reallocates a memory block using malloc and free

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -3,44 +3,39 @@
 
 /**
  * _realloc - Reallocates a memory block using malloc and free.
- *
  * @ptr: Pointer to the memory previously allocated with malloc(old_size)
- *
  * @old_size: Size of the allocated space for ptr, in bytes
- *
  * @new_size: New size of the memory block, in bytes
  *
  * Return: Pointer to the newly allocated memory block
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-
 {
-    void *new_ptr;
-    unsigned int min_size;
-    unsigned int i;
+	void *new_ptr;
+	unsigned int min_size;
+	unsigned int i;
 
-    if (new_size == old_size)
-        return (ptr);
+	if (new_size == old_size)
+		return (ptr);
 
-    if (ptr == NULL)
-        return (malloc(new_size));
+	if (ptr == NULL)
+		return (malloc(new_size));
 
-    if (new_size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-    new_ptr = malloc(new_size);
-    if (new_ptr == NULL)
-        return (NULL);
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
 
-    min_size = old_size < new_size ? old_size : new_size;
-    for (i = 0; i < min_size; i++)
-        *((char *)new_ptr + i) = *((char *)ptr + i);
+	min_size = old_size < new_size ? old_size : new_size;
+	for (i = 0; i < min_size; i++)
+		*((char *)new_ptr + i) = *((char *)ptr + i);
 
-    free(ptr);
+	free(ptr);
 
-    return (new_ptr);
+	return (new_ptr);
 }
