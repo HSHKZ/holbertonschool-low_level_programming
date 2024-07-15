99d16de030eb0972795916f5eae64b49da9d32bd
HSHKZ <lucasolegniel@gmail.com>
2024-07-15 15:00:02 +0200

    program that prints the opcodes of its own main function.

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -1,57 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
+	#include <stdlib.h>
 
-/**
- * print_opcodes - Prints the opcodes of a given function.
- * @func: Pointer to the function.
- * @n: Number of bytes to print.
- *
- * Description: This function prints the opcodes of the specified
- * function in hexadecimal format.
- */
-void print_opcodes(void *func, int n)
-{
-    int i;
-    unsigned char *p = (unsigned char *)func;
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%02x", p[i]);
-        if (i < n - 1)
-        {
-            printf(" ");
-        }
-    }
-    printf("\n");
-}
+	/**
+	 * main - prints its own opcodes
+	 * @argc: number of arguments
+	 * @argv: array of arguments
+	 *
+	 * Return: Always 0 (Success)
+	 */
+	int main(int argc, char *argv[])
+	{
+		int bytes, i;
+		char *arr;
 
-/**
- * main - Entry point of the program.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line arguments.
- *
- * Return: 0 on success, 1 on incorrect number of arguments, 2 on negative byte count.
- *
- * Description: This program prints the opcodes of its own main function.
- * The number of bytes to print is specified as a command-line argument.
- */
-int main(int argc, char *argv[])
-{
- int n;
 
- if (argc != 2)
- {
- 	printf("Error\n");
- 	return (1);
- }
+		if (argc != 2)
+		{
+			printf("Error\n");
+			exit(1);
+		}
 
- n = atoi(argv[1]);
- if (n < 0)
- {
- 	printf("Error\n");
- 	return (2);
- }
 
- print_opcodes((void *)main, n);
- return (0);
-}
+		bytes = atoi(argv[1]);
+
+
+		if (bytes < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+
+
+		arr = (char *)main;
+
+
+		for (i = 0; i < bytes; i++)
+		{
+			if (i == bytes - 1)
+			{
+				printf("%02hhx\n", arr[i]);
+				break;
+			}
+			printf("%02hhx ", arr[i]);
+		}
+		return (0);
+	}
