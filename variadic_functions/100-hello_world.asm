a0cb49adc877782920d6e536ca88c3be50fc14f1
HSHKZ <lucasolegniel@gmail.com>
2024-07-17 16:21:07 +0200

    a 64-bit program in assembly that prints Hello, World, followed by a new line.

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,20 @@
+; File: 100-hello_world.asm
+; Desc: 64-bit assembly program that prints
+;       Hello, World followed by a new line.
+
+section .text
+   global main
+
+main:
+   mov edx,len
+   mov ecx,msg
+   mov ebx,1
+   mov eax,4
+   int 0x80
+
+   mov eax,0
+   int 0x80
+
+section .data
+   msg: db 'Hello, World', 0xa
+   len: equ $ - msg
