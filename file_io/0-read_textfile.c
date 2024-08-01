73a7f71cf341537debd5d902924c8047e4a538d5
HSHKZ <lucasolegniel@gmail.com>
2024-08-01 10:27:20 +0200

    function that reads a text file and prints it to the POSIX standard output.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,45 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to
- *                 the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters it should read and print.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: The actual number of letters it could read and print, or 0 if:
- *         - The file cannot be opened or read.
- *         - filename is NULL.
- *         - Write fails or does not write the expected amount of bytes.
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int bytes_read = 0;
-    char c;
-    int descriptor;
+	ssize_t o, r, w;
+	char *buffer;
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
+	if (filename == NULL)
+		return (0);
 
-    descriptor = open(filename, O_RDONLY);
-    if (descriptor == -1)
-    {
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    while (letters > 0 && read(descriptor, &c, 1) == 1)
-    {
-        if (write(STDOUT_FILENO, &c, 1) != 1)
-        {
-            close(descriptor);
-            return (0);
-        }
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-        bytes_read++;
-        letters--;
-    }
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    close(descriptor);
-    return (bytes_read);
+	free(buffer);
+	close(o);
+
+	return (w);
 }
