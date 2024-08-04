15e279153660fdb97ab0d7c6017f406c796e9fb6
HSHKZ <lucasolegniel@gmail.com>
2024-08-04 22:04:20 +0200

    main.h file

--- file_io/main.h
+++ file_io/main.h
@@ -1,16 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
-#define _GNU_SOURCE
+#ifndef main_h
+#define main_h
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<sys/types.h>
-#include<sys/stat.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <libgen.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+
 int create_file(const char *filename, char *text_content);
+
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+int _putchar(char a);
+
+#endif /* main_h */
